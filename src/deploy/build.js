40a3dfc9c94b41cba57747ad5f8f78ddf615c79c
Sergey Kovalev <enasik@gmail.com>
2017-10-30 18:27:28 +0200

    Added: spawn revert

--- src/deploy/build.js
+++ src/deploy/build.js
@@ -1,13 +1,13 @@
 import {Spinner} from 'cli-spinner'
 import clc from 'cli-color'
-import {spawn} from '../lib'
+import {exec} from 'child-process-promise'
 
 export default async () => {
   let spinner = new Spinner('%s Building app...')
   spinner.setSpinnerString('⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏')
   spinner.start()
 
-  await spawn('npm run build')
+  await exec('npm run build')
 
   spinner.stop(true)
 
