767c4e7d4a181b11d2b05dfedfb1503825cccd9c
Sergey Kovalev <enasik@gmail.com>
2017-10-30 19:21:36 +0200

    Added: test

--- src/deploy/build.js
+++ src/deploy/build.js
@@ -1,13 +1,37 @@
 import {Spinner} from 'cli-spinner'
 import clc from 'cli-color'
-import {spawn} from 'child-process-promise'
+// import {spawn} from 'child-process-promise'
+
+const runBuild = () => {
+  return new Promise((resolve, reject) => {
+    const child = process.spawn('npm run build', [], { cwd: process.cwd(), env: process.env });
+
+    child.stdout.on('data', function (data) {
+      console.log('stdout: ' + data);
+    });
+
+    child.stderr.on('data', function (data) {
+      console.log('stderr: ' + data);
+    });
+
+    child.on('close', function (code) {
+      console.log('child process exited with code ' + code);
+      if (Number(code) === 0) {
+        resolve(code);
+      } else {
+        reject(code);
+      }
+    });
+  })
+};
 
 export default async () => {
   let spinner = new Spinner('%s Building app...')
   spinner.setSpinnerString('⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏')
   spinner.start()
 
-  await spawn('npm run build', [], { cwd: process.cwd(), env: process.env })
+  // await spawn('npm run build', [], { cwd: process.cwd(), env: process.env })
+  await runBuild()
 
   spinner.stop(true)
 
